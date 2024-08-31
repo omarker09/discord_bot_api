b79296b013348a34f743abc02861ed290772f4de
Omar_09 <brigades.anony99@gmail.com>
2024-09-01 00:54:23 +0100

    add some changes

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
 const app = express();
 const port = 5000;
+dotenv.config();
 app.listen(port, () => {
     return console.log(`server is running on port ${port}`);
 });
 app.get("/", (req, res) => {
-    return res.status(200).json({ message: "well done!" });
+    return res.status(200).json({ message: "well done!", discordToken: process.env.DISCORD_BOT_TOKEN });
 });
