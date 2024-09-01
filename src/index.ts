001cc67ee4a6fa635a14db13831bf8a68981db75
Omar_09 <brigades.anony99@gmail.com>
2024-09-01 01:15:33 +0100

    init the bot

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,28 @@ import encryptionService from "./services/encryption.service";
 import authController from "./controllers/auth.controller";
 import express from "express"
 import dotenv from "dotenv"
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits, Interaction } from 'discord.js';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const token = "qldj8QEEdK16NHx0-xJUBE_t6imHfNL_"
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on('interactionCreate', async (interaction: Interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+});
+
+client.login(token);
 
 const app = express();
 const port = 5000;
 dotenv.config();
-
 
 app.listen(port, () => {
     return console.log(`server is running on port ${port}`);
@@ -18,19 +34,3 @@ app.get("/", (req, res) => {
         message: "well done!", discordToken: process.env.DISCORD_BOT_TOKEN
     })
 })
-
-const client: any = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('interactionCreate', async (interaction: any) => {
-    if (!interaction.isChatInputCommand()) return;
-
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
-    }
-});
-
-client.login(process.env.DISCORD_BOT_TOKEN);
