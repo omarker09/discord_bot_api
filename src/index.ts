eed5648a029921da6ac2fb90c6c6a5e8c12d94e6
Omar_09 <brigades.anony99@gmail.com>
2024-09-01 01:08:04 +0100

    init the bot

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import encryptionService from "./services/encryption.service";
 import authController from "./controllers/auth.controller";
 import express from "express"
 import dotenv from "dotenv"
+import { Client, GatewayIntentBits } from 'discord.js';
 
 const app = express();
 const port = 5000;
@@ -17,3 +18,19 @@ app.get("/", (req, res) => {
         message: "well done!", discordToken: process.env.DISCORD_BOT_TOKEN
     })
 })
+
+const client: any = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async (interaction: any) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
